68de44ba565b9209e899f961b98967df1fdbf952
Luis Pedro Coelho <luis@luispedro.org>
2012-05-10 16:51:43 +0100

    ENH Generic instance for Binary

--- Data/Binary/Generic.hs
+++ Data/Binary/Generic.hs
@@ -1,16 +1,33 @@
+{-# LANGUAGE TypeOperators, FlexibleContexts #-}
+module Data.Binary.Generic () where
 
+import Data.Binary
+import GHC.Generics
 
-class GSerialize f where
-  gput :: f a -> [Bin]
-instance GSerialize U1 where
-  gput U1 = []
-instance (GSerialize a, GSerialize b) => GSerialize (a :*: b) where
-  gput (a :*: b) = gput a ++ gput b
+instance Binary (U1 p) where
+  put U1 = return ()
+  get = return U1
 
-instance (GSerialize a, GSerialize b) => GSerialize (a :+: b) where
-  gput (L1 x) = O : gput x
-  gput (R1 x) = I : gput x
+instance (Binary (f p), Binary (g p)) => Binary ((:*:) f g p) where
+  put (a :*: b) = put a >> put b
+  get = do
+    a <- get
+    b <- get
+    return (a :*: b)
 
-instance (GSerialize a) => GSerialize (M1 i c a) where
-  gput (M1 x) = gput x
-  gput (K1 x) = put x
+instance (Binary (f p), Binary (g p)) => Binary ((:+:) f g p) where
+  put (L1 x) = put False >> put x
+  put (R1 x) = put True >> put x
+  get = do
+    v <- get :: Get Bool
+    if v
+        then get >>= (return . L1)
+        else get >>= (return . R1)
+
+instance (Binary (f p)) => Binary (M1 i c f p) where
+  put (M1 x) = put x
+  get = get >>= (return . M1)
+
+instance (Binary c) => Binary (K1 i c p) where
+  put (K1 x) = put x
+  get = get >>= (return . K1)
